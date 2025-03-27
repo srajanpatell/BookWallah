9f16c720bf301b237d732036b567128625d1338d
Srajan Patel <srajanpatell04@gmail.com>
2025-03-27 17:40:19 +0530

    My Bookwallah Commit

---
+++ BookWallah/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Landing from './components/Landing'
+import Featured from './components/Featured'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Landing />
+      <Featured />
+    </>
+  )
+}
+
+export default App
